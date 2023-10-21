ecd1426e6f650cd10c5ff8b0f385fc4fea634af2
Connor Rhodes <connor@rhodes.contact>
2023-10-21 20:20:54 +0000

    added fish aliases

---
+++ private_dot_config/private_fish/aliases.fish
@@ -0,0 +1,14 @@
+# programs
+alias e="nvim"
+alias cm="chezmoi"
+alias ccd="SHELL=/usr/bin/fish chezmoi cd"
+
+# navigation
+alias j="__zoxide_z"
+alias ji="__zoxide_zi"
+
+# git
+alias gs="git status"
+
+# commands
+alias x=exit
