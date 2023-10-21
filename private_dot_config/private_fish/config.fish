6421e1daca852d305ebc74fc815e8b730db1b88e
Connor Rhodes <connor@rhodes.contact>
2023-10-21 20:24:28 +0000

    enable fish vi mode

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,4 +1,8 @@
 if status is-interactive
+    fish_vi_key_bindings
+    set -g fish_greeting
+    # disable vi mode in fish prompt
+    function fish_mode_prompt; end
     # Commands to run in interactive sessions can go here
     source $HOME/.config/fish/aliases.fish
 end
