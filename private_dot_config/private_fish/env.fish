c89d9e990953278ec91a59c1e36fe39f3ff6e9fd
Connor Rhodes <connor@rhodes.contact>
2023-10-24 03:35:03 +0000

    added cargo bin to path

--- private_dot_config/private_fish/env.fish
+++ private_dot_config/private_fish/env.fish
@@ -1,3 +1,3 @@
 # paths
-set --export PATH $HOME/.local/bin $HOME/.local/share/bin $PATH
+set --export PATH $HOME/.local/bin $HOME/.local/cargo/bin $HOME/.local/share/bin $PATH
 set --export CARGO_HOME $HOME/.local/cargo
