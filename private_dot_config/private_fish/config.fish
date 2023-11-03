5897f2427a264eeb6cd44acd13f38f83cee660f6
Connor Rhodes <connor@rhodes.contact>
2023-11-03 13:46:54 +0000

    added vi mode prompt config (commented)

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -10,5 +10,14 @@ end
 
 source $HOME/.config/fish/env.fish
 
+# Set the cursor shapes for the different vi modes.
+## remove [I] [N] indicators
+#function fish_mode_prompt; end
+## define cursor shape in each of the vi modes
+#set fish_cursor_default     block
+#set fish_cursor_insert      line
+#set fish_cursor_replace_one underscore
+#set fish_cursor_visual      block
+
 # functions
 source $HOME/.config/fish/functions/zoxide.fish
