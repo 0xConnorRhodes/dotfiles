5ef3242320ba7d7c3b0bc5a60a1efb0a3b632dd4
Connor Rhodes <connor@rhodes.contact>
2024-05-18 10:34:09 -0500

    exported locale configuration for nix

--- private_dot_config/private_fish/env.fish
+++ private_dot_config/private_fish/env.fish
@@ -1,3 +1,4 @@
 # paths
 set --export PATH $HOME/.local/bin $HOME/.local/cargo/bin $HOME/.local/share/bin $PATH
 set --export CARGO_HOME $HOME/.local/cargo
+set --export LOCALE_ARCHIVE /usr/lib/locale/locale-archive # needed for nix to get locale
