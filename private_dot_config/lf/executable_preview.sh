99ea0c67376ffacea67a07bad513fa3163beb152
Connor Rhodes <connor@rhodes.contact>
2024-05-22 23:14:13 +0000

    updated scripts and configs for bat and fd symlinks

--- private_dot_config/lf/executable_preview.sh
+++ private_dot_config/lf/executable_preview.sh
@@ -3,14 +3,8 @@
 # allows bat to use color in lf
 unset COLORTERM
 
-if [ -f /usr/bin/batcat ]; then
-  BAT_BIN="/usr/bin/batcat"
-else
-  BAT_BIN="/usr/bin/bat"
-fi
-
 case "$1" in
 	*.md|*.txt|*.org) bat --color=always --style=plain "$1";;
 	*.mkv|*.mp4|*.webm) mediainfo "$1";;
-	*) $BAT_BIN --color=always --style=plain "$1";;
+	*) bat --color=always --style=plain "$1";;
 esac
--- private_dot_config/private_fish/env.fish
+++ private_dot_config/private_fish/env.fish
@@ -1,3 +1,2 @@
 # paths
-set --export PATH $HOME/.local/bin $HOME/.local/cargo/bin $HOME/.local/share/bin $PATH
-set --export CARGO_HOME $HOME/.local/cargo
+set --export PATH $HOME/.local/bin $PATH
