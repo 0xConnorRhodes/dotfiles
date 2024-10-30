95a8cb9b032cd28cd4d78b8506b09dd24c3dd798
Connor Rhodes <connor@rhodes.contact>
2024-10-29 20:56:06 -0500

    add invisibles comment

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,4 +1,6 @@
 require("config.lazy")
 
 vim.opt.mouse = 'a' -- add mouse support
+
+-- 0 doesn't hide syntax chars, 1 hides but keeps the space they occupy, 2 hides blank space and shifts text when those chars are shown
 vim.opt.conceallevel = 1 -- needed for obsidian-nvim
