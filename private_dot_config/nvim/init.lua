7a39a62a9349a106a1808cd9aea7d7f28d8e3dba
Connor Rhodes <connor@rhodes.contact>
2024-10-29 20:44:58 -0500

    add nvim lazy and obsidian-nvim config

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+
+vim.opt.mouse = 'a' -- add mouse support
+vim.opt.conceallevel = 1 -- needed for obsidian-nvim
---
+++ private_dot_config/nvim/lua/plugins/obsidian-nvim.lua
@@ -0,0 +1,25 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",  -- recommended, use latest release instead of latest commit
+  lazy = true,
+  -- ft = "markdown", -- load on all markdown filetypes
+  event = {
+    -- the ~/notes/... path only works when that path to the symlinked directory is specified. eg ~/notes/file.md
+    -- "BufReadPre "..vim.fn.expand "~".."/notes/**.md",
+    "BufReadPre /nmount/box/notes/**.md",
+    -- "BufNewFile "..vim.fn.expand "~".."/notes/**.md",
+    "BufNewFile /nmount/box/notes/**.md",
+  },
+  dependencies = {
+    -- Required.
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/notes",
+      }
+    },
+  },
+}
