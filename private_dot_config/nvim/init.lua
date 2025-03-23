32b354cb237eb8cfc7fc91069faafa6aa7260f54
Connor Rhodes <connor@rhodes.contact>
2025-03-22 19:38:33 -0500

    disable f1 in vimrc

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -4,3 +4,8 @@ vim.opt.mouse = 'a' -- add mouse support
 
 -- 0 doesn't hide syntax chars, 1 hides but keeps the space they occupy, 2 hides blank space and shifts text when those chars are shown
 vim.opt.conceallevel = 0 -- needed for obsidian-nvim
+
+-- Disable F1
+vim.api.nvim_set_keymap('n', '<F1>', '<nop>', { noremap = true, silent = true }) -- normal mode
+vim.api.nvim_set_keymap('i', '<F1>', '<nop>', { noremap = true, silent = true }) -- insert mode
+
