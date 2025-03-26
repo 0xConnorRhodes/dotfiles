c901526eaacb8a22e565f9e8986f6170970d3b0c
Connor Rhodes <connor@rhodes.contact>
2025-03-26 08:11:18 -0500

    add ruby path

--- private_dot_config/private_fish/env.fish
+++ private_dot_config/private_fish/env.fish
@@ -1,3 +1,3 @@
 # paths
-set -x PATH $HOME/code/scripts $HOME/.local/bin $PATH
+set -x PATH $HOME/code/scripts $HOME/code/scripts/priv $HOME/.local/bin $HOME/.local/share/gem/ruby/3.3.0/bin $PATH
 set -x EDITOR nvim
